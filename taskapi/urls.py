d4876eb7ad178ef6827988831aaeef40642aa0b7
vmahkook <130460895+vmahkook@users.noreply.github.com>
2026-03-08 10:57:22 +0530

    Create urls.py

---
+++ taskapi/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # JWT auth
+    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Tasks
+    path('api/', include('tasks.urls')),
+]
